805864f7e974ba32d08305e18f12f9693ba6c28f
me <salinagormez@gmail.com>
2026-08-10 15:42:39 +0530

    day 2 codes of the lab

---
+++ 10-08-2026/bianary-recursion.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int bianarySearch(int arr[],int x,int low,int high){
+    while(high>=low){
+        int mid=low+(high - low)/2;
+        if(arr[mid]==x){
+            return mid;
+
+        }
+        else if(x>arr[mid]){
+            return bianarySearch(arr,x,mid+1,high);
+        }
+
+        else{
+           return  bianarySearch(arr,x,low,mid-1);
+            }
+
+    }
+
+    return -1;
+
+}
+
+
+int main(){
+    int arr[]={10,20,30,40,50,60,70,80};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int x=70;
+    int result=bianarySearch(arr,x,0,n-1);
+    if (result==-1){
+        printf("not found");
+    }
+    else{
+        printf("found at index %d",result);
+    }
+    return 0;
+
+}
